aeb6549f507584e9e4ac4a96c5f86443274c6a2f
KOKILADEVI.A <kokiladevi008@gmail.com>
2026-09-04 23:17:42 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://safedarshan-m0du.onrender.com';
 
 export async function apiFetch(endpoint, options = {}) {
   const url = `${API_BASE_URL}${endpoint}`;
